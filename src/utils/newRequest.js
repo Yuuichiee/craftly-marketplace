3e1b065b9c330aaeab71ce22626745c0e13b1df1
shivachauhan98171-cloud <shivachauhan98171@gmail.com>
2026-08-26 14:55:49 +0530

    Set production backend URL to https://craftly-marketplace-backend2.onrender.com/api

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8800/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://craftly-marketplace-backend2.onrender.com/api",
   withCredentials: true,
 });
 
